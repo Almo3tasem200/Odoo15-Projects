cbb318d63ffff5c95d8087ff84da17b396172a89
Almo3tasem200 <moatasem.elemam20@gmail.com>
2026-06-07 02:49:18 +0300

    initial commit

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Hospital Management' ,
+    'version': '1.0',
+    'category': 'Hospital',
+    'author': 'Almoatasem',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """ Hospital management system """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license' : 'LGPL-3'
+}
