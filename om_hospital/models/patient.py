2f8bccc1ab920b34f3db7fcdfc80bce8bddfe9a8
Almo3tasem200 <moatasem.elemam20@gmail.com>
2026-06-07 03:35:27 +0300

    updating models

--- om_hospital/models/patient.py
+++ om_hospital/models/patient.py
@@ -3,4 +3,10 @@ from odoo import api, fields, models
 class HospitalPatient(models.Model):
     _name = "hospital.patient" #hospital_patient table in db
     _description = "Hospital Patient"
-    
+
+    name = fields.Char(string="Patient Name")
+    age = fields.Integer(string="Age")
+    gender = fields.Selection([
+        ("male", "Male"),
+        ("female", "Female")
+    ],string="Gender")
